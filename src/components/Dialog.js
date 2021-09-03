bbbafe41e3280a7fde42c80ef211c345d01ff9a1
Uğur Yılmaz <ugr-yilmaz@yandex.com>
2021-09-03 17:38:57 +0300

    dialog arayüzü iyileştirildi.

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -26,16 +26,16 @@ export default function CompDialog(props) {
       <ButtonGroup variant="text" aria-label="text button group">
         <Button onClick={handleOpen}>{props.user.name}</Button>
       </ButtonGroup>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={open}>
         <DialogTitle>{props.user.name}</DialogTitle>
         <DialogContent>
           <DialogContentText>{props.user.desc}</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} variant="contained">
+          <Button onClick={handleClose} variant="text">
             Reddet
           </Button>
-          <Button onClick={handleClose} variant="contained">
+          <Button onClick={handleClose} variant="text">
             Onayla
           </Button>
         </DialogActions>
